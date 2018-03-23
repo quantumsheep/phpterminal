2eedf9aad4ce410b0a008dc6a08ccf8227f40eac
Mickal decelle <mickaeldecelle@hotmail.fr>
2018-03-23 17:17:21 +0100

    Terminal.js fonctionnel.

--- www/app/assets/js/terminal.js
+++ www/app/assets/js/terminal.js
@@ -2,6 +2,8 @@ const conn = new WebSocket('ws://localhost:800');
 let HistoryCmd = [""];
 let HistoryCount = 0;
 let HistoryCounter = 0;
+let termContainer = document.getElementById("terminal-container");
+let ClickCount = 0;
 
 conn.onopen = (e) => {
     console.log("Connection established!");
@@ -54,121 +56,61 @@ document.getElementById('terminal-input').addEventListener('keydown', (e) => {
     }
 });
 
-document.getElementById("terminal-container").addEventListener("mousedown", e => {
+function move() {
+    console.log("Move");
+};
 
-    function MouseMove() {
-        console.log("move");
-        //  document.getElementById("terminal-container").removeEventListener("mouseup", MouseUp, false);
+function click() {
+    ClickCount++;
+    if (ClickCount == 1) {
+        singleClickTimer = setTimeout((f) => {
+            let input = document.getElementById('terminal-input');
+
+            let range;
+            let selection;
+
+            if (document.createRange) //Firefox, Chrome, Opera, Safari, IE 9+
+            {
+                range = document.createRange();
+                range.selectNodeContents(input);
+                range.collapse(false);
+                selection = window.getSelection();
+                selection.removeAllRanges();
+                selection.addRange(range);
+            }
+            else if (document.selection) //IE 8 and lower
+            {
+                range = document.body.createTextRange();
+                range.moveToElementText(input);
+                range.collapse(false);
+                range.select();
+            }
+            ClickCount = 0;
+        }, 200);
+    } else if (ClickCount > 1) {
+        clearTimeout(singleClickTimer);
+        ClickCount = 0;
     }
+};
 
-    function click(){
-        console.log("click");
-    }
+termContainer.addEventListener("click", click, false);
 
-    function MouseUp() {
-        console.log("mouseup");
-        clearTimeout(selectTimer);
+termContainer.addEventListener("mousedown", e => {
+    termContainer.addEventListener("click", click, false);
+    mouseDown = setTimeout((f) => {
+        termContainer.addEventListener("mousemove", move, false);
+        termContainer.removeEventListener("click", click, false);
 
-        document.getElementById("terminal-container").removeEventListener("mousemove", MouseMove, false);
-
-        let input = document.getElementById('terminal-input');
-
-        let range;
-        let selection;
-
-        if (document.createRange) //Firefox, Chrome, Opera, Safari, IE 9+
-        {
-            range = document.createRange();
-            range.selectNodeContents(input);
-            range.collapse(false);
-            selection = window.getSelection();
-            selection.removeAllRanges();
-            selection.addRange(range);
-        }
-        else if (document.selection) //IE 8 and lower
-        {
-            range = document.body.createTextRange();
-            range.moveToElementText(input);
-            range.collapse(false);
-            range.select();
-        }
-    }
-
-    console.log("mousedown");
-
-    selectTimer = setTimeout((e) => {
-        document.getElementById("terminal-container").addEventListener("mousemove", MouseMove, false);
-    }, 1000);
-
-    document.getElementById("terminal-container").addEventListener("mouseup", MouseUp, false);
+    }, 200);
+    termContainer.addEventListener("mouseup", e => {
+        clearTimeout(mouseDown);
+        termContainer.removeEventListener("mousemove", move, false);
+    });
 });
-
-// document.getElementById('terminal-user').addEventListener('mousedown', (e) => {
-//     MouseDown++;
-//     if (MouseDown == 2) {
-//         selectTimer = setTimeout((f) => {
-//             document.getElementById('terminal-container').addEventListener('click', (e) => {
-//                 ClickCount++;
-//                 if (ClickCount == 1) {
-//                     singleClickTimer = setTimeout((f) => {
-//                         let input = document.getElementById('terminal-input');
-
-//                         let range;
-//                         let selection;
-
-//                         if (document.createRange) //Firefox, Chrome, Opera, Safari, IE 9+
-//                         {
-//                             range = document.createRange();
-//                             range.selectNodeContents(input);
-//                             range.collapse(false);
-//                             selection = window.getSelection();
-//                             selection.removeAllRanges();
-//                             selection.addRange(range);
-//                         }
-//                         else if (document.selection) //IE 8 and lower
-//                         {
-//                             range = document.body.createTextRange();
-//                             range.moveToElementText(input);
-//                             range.collapse(false);
-//                             range.select();
-//                         }
-//                         ClickCount = 0;
-//                     }, 200);
-//                 } else if (ClickCount > 1) {
-//                     clearTimeout(singleClickTimer);
-//                     ClickCount = 0;
-//                 }
-//             }, 20);
-//             selectingTimer = setTimeout((f) => {
-//                 document.getElementById('terminal-user').addEventListener('mousemove', (e) => {
-//                     CountDown++;
-//                     console.log("TRIGGER2 : " + CountDown);
-//                     if (CountDown == 2) {
-//                         stopSelectClickTimer = setTimeout((f) => {
-//                             document.getElementById('terminal-user').addEventListener('mouseup', (e) => {
-//                                 console.log("TRIGGER3 : " + CountDown);
-//                                 CountDown = 0;
-//                             }, false);
-//                         }, 10);
-//                         CountDown == 0;
-//                     } else if (CountDown == 0) {
-//                         clearTimeout(selectClickTimer);
-//                         clearTimeout(stopSelectClickTimer);
-//                         CountDown = 0;
-//                     }
-//                 }, 200);
-//             }, false);
-//         }, false);
-//     } else if (MouseDown > 1) {
-//         clearTimeout(selectTimer);
-//         clearTimeout(selectingTimer);
-//         MouseDown = 0;
-//     }
-// }, false);
 
 conn.onmessage = (e) => {
     appendTerminal(e.data);
-    document.getElementById("terminal-container").scrollTo(0, document.getElementById("terminal-container").scrollHeight);
+    termContainer.scrollTo(0, termContainer.scrollHeight);
 };
 
 function appendTerminal(text) {
