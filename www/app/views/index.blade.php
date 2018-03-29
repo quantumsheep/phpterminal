9bf53e75bb7dfdcb350c106ac2a3eb0f45b81bc1
Mickal decelle <mickaeldecelle@hotmail.fr>
2018-03-29 16:02:30 +0200

    Perfect index animation

--- www/app/views/index.blade.php
+++ www/app/views/index.blade.php
@@ -15,27 +15,27 @@
         </div>
     </div>
 
-    <div id="term-test" class="terminal container">
-    <div class="terminal container" id="terminal-container-test">
-        <div id="term">
-            <div class="terminal-content" id="terminal-content-user-test">
-                <div id="terminal-content-response-test">
-                    <div id="terminal-user-test">user@user:~ $
-                        <span class="terminal-input" id="terminal-input-test" contenteditable="false" spellcheck="false"></span>
+    <div class="terminal container" id="term-test">
+        <div class="terminal container" id="terminal-container-test">
+            <div id="term">
+                <div class="terminal-content" id="terminal-content-user-test">
+                    <div id="terminal-content-response-test">
+                        <div id="terminal-user-test">user@user:~ $
+                            <span class="terminal-input" id="terminal-input-test" contenteditable="false" spellcheck="false"></span>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    </div>
 
-    <div class="terminal container shy" id="terminal-container">
-        <div class="terminal-content" id="terminal-content-user">
+    <div class="terminal container shy" id="terminal-container-exemple">
+        <div class="terminal-content" id="terminal-content-user-exemple">
             </div>
-            <div id="terminal-content-response">
-            <div id="terminal-user">user@user:~ $
-                <span class="terminal-input" id="terminal-input" contenteditable="true" spellcheck="false"></span>
-                <span class="terminal-caret">█</span>
+            <div id="terminal-content-response-exemple">
+            <div id="terminal-user-exemple" class="shy" >anonymous@demoterminal:~ $
+                <span class="terminal-input" id="terminal-input-exemple" contenteditable="true" spellcheck="false"></span>
+                <span id="caret" class="terminal-caret">█</span>
             </div>
         </div>
     </div>
