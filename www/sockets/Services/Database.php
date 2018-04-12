03c97066a92cce27b205384ed333ba3fadfa2585
Unknown <halimisimon@gmail.com>
2018-04-12 14:48:30 +0200

    has to 2

--- www/sockets/Services/Database.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Alph\Services;
-
-class Database {
-    public static function connect() {
-        return new \PDO('mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
-    }
-}
