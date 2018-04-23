4bcee41208ff0a6766b9e6f00e80908672519105
QuantumSheep <quantumsheep@outlook.fr>
2018-04-23 12:48:42 +0200

    New command client handling system

---
+++ www/sockets/src/commands/clear.php
@@ -0,0 +1,18 @@
+<?php
+namespace Alph\Commands;
+
+use Alph\Services\CommandInterface;
+use Alph\Services\DefinedCommands;
+use Ratchet\ConnectionInterface;
+use Alph\Services\SenderData;
+
+class clear implements CommandInterface
+{
+    /**
+     * Call the command
+     */
+    public static function call(\PDO $db, \SplObjectStorage $clients, SenderData &$data, ConnectionInterface $sender, string $sess_id, array $sender_session, string $terminal_mac, string $cmd, $parameters)
+    {
+        $sender->send("action|clear");
+    }
+}
