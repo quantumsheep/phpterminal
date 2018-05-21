47b2c5758c87620c24cdd9168e7132f14f9d8024
Simonpo95 <33205321+Simonpo95@users.noreply.github.com>
2018-05-22 01:17:57 +0200

    mkdir reworking

--- www/sockets/src/commands/mkdir.php
+++ www/sockets/src/commands/mkdir.php
@@ -35,118 +35,60 @@ class mkdir implements CommandInterface
     public static function call(\PDO $db, \SplObjectStorage $clients, SenderData &$data, ConnectionInterface $sender, string $sess_id, array $sender_session, string $terminal_mac, string $cmd, $parameters)
     {
         $basicmod = 777;
-        $newDirs = [];
         $params = "";
-        $idDirectory = 0;
-        $arrayFullPath = [];
+        $positionDir = "KO";
+        $sended_path = "";
 
         // If no params
         if (empty($parameters)) {
             $sender->send("message|<br>Opérande manquant<br>Saisissez mkdir --help pour plus d'information");
             return;
-        }
-
-        // Get position by current directory name
-        if ($data->position == "/") {
-            $sender->send("message|" . $idDirectory);
-            $positionDir = null;
         } else {
-            $position = explode("/", $data->position);
-            $positionDir = $position[count($position) - 1];
-        }
 
-        //check for "" case
-        preg_match_all("/ (\"([^\"]*)\") /", " " . $parameters . " ", $quotedParams);
+            preg_match_all("/\"[^\"]*\"/", $parameters, $quotedParams);
 
-        //rearrange params in case of "" case
-        if (!empty($quotedParams[1])) {
-            foreach ($quotedParams[1] as $quotedParameter) {
-                $parameters = str_replace($quotedParameter, "", $parameters);
-            }
-        }
+            if (!empty($quotedParams[0])) {
 
-        // -d parameters multiple creation case
-        preg_match_all("/ ((\/\"[^\"]+[\"]?\")|(\/[^\"\/ ]+))+\/? /", " " . $parameters . " ", $stringFullPath);
-        if (!empty($stringFullPath[0])) {
-            $sender->send("message|" . $stringFullPath[0][0]);
-
-            // Get elements from regex
-            for ($i = 0; $i < count($stringFullPath[0]); $i++) {
-                $arrayFullPath[$i] = explode("/", $stringFullPath[0][$i]);
-                for ($j = 1; $j < count($arrayFullPath[$i]); $j++) {
-                    $arrayFullPath[$i] = str_replace($arrayFullPath[$i], "\"", "");
-                    $sender->send("message|" . $arrayFullPath[$i][0]);
+                for ($i = 0; $i < sizeof($quotedParams); $i++) {
+                    $tmp[$i] = $quotedParams[0][$i];
                 }
 
-            }
-            return;
-        }
+                $str = implode($tmp);
 
-        // Table of new directory with $paramParts
-        $paramParts = explode(" ", $parameters);
-        if (!empty($paramParts)) {
-            for ($i = 0; $i < count($paramParts); $i++) {
-                if (!empty($paramParts[$i]) && $paramParts[$i][0] != '-') {
-                    $newDirs[] = $paramParts[$i];
-                } else {
-
-                    // Get parameters
-                    $params .= $paramParts[$i];
+                foreach ($tmp as $value) {
+                    $parameters = str_replace($value, "", $parameters);
                 }
-            }
-        }
 
-        // Add the quotedParams to the new directories list
-        if (!empty($quotedParams[2])) {
-            for ($i = 0; $i < count($quotedParams[2]); $i++) {
-                $newDirs[] = $quotedParams[2][$i];
-            }
-        }
+                $str = str_replace(" ", "_", $str);
 
-        // Get parameters
-        $paramLetters = explode("-", $params);
+                $parameters .= " " . $str;
 
-        foreach ($newDirs as $name) {
-
-            // case directory's name is /
-            if ($name == "/") {
-                $sender->send("message|<br>" . $positionDir . ":Impossible to create / directory. The directory already exists.");
+                $parameters = str_replace('"', "", $parameters);
             }
 
-            //Convert relative position name to IdDirectory
-            if (!empty($positionDir)) {
-                $getIdDirectory = $db->prepare("SELECT iddir FROM TERMINAL_DIRECTORY WHERE name = :daddy");
-                $getIdDirectory->bindParam(":daddy", $positionDir);
-                if ($getIdDirectory->execute()) {
-                    if ($getIdDirectory->rowCount() > 0) {
-                        $idDirectory = $getIdDirectory->fetch(\PDO::FETCH_ASSOC)["iddir"];
-                    }
-                }
-            }
+            // Get parameters
+            $paramList = explode(" ", $parameters);
 
-            // Case Directory already exists in current directory
-            $check = $db->prepare("SELECT name FROM terminal_directory WHERE name = :name AND parent = :daddy");
-            $check->bindParam(":name", $name);
-            $check->bindParam(":daddy", $idDirectory);
-            $check->execute();
-            if ($check->rowCount() > 0) {
-                $sender->send("message|<br>" . $positionDir . ":" . $name . " directory already exists");
+            foreach ($paramList as $name) {
 
-            } else if (strlen($name) > 255) {
+                // Get actual directory ID
+                $getIdDirectory = $db->prepare("SELECT IdDirectoryFromPath(:paths, :mac) as id");
+                $getIdDirectory->bindParam(":mac", $terminal_mac);
+                $getIdDirectory->bindParam(":paths", $data->position);
+                $getIdDirectory->execute();
+                $CurrentDir = $getIdDirectory->fetch(\PDO::FETCH_ASSOC)["id"];
+                
 
-                // Case directory name exceed 255 char
-                $sender->send("message|<br>" . $positionDir . ": one of the directories' name is too long. It must not exceed 255 characters.");
+                $pathlist = explode('/', $name);
 
-            } else {
-
-                // Case everything matches
+                $name = $pathlist[count($pathlist) - 1];
 
                 // Prepare
-                $stmp = $db->prepare("INSERT INTO TERMINAL_DIRECTORY(terminal, parent, name, chmod, owner, `group`, createddate, editeddate) VALUES(:terminal, :parent, :name, :chmod, :owner, (SELECT gid FROM terminal_user WHERE idterminal_user = :owner), NOW(),NOW());");
+                $stmp = $db->prepare("INSERT INTO terminal_directory(terminal, parent, name, chmod, owner, `group`, createddate, editeddate) VALUES(:terminal, :parent, :name, :chmod, :owner, (SELECT gid FROM terminal_user WHERE idterminal_user = :owner), NOW(),NOW());");
 
                 // Bind parameters put in SQL
                 $stmp->bindParam(":terminal", $terminal_mac);
-                $stmp->bindParam(":parent", $idDirectory);
+                $stmp->bindParam(":parent", $CurrentDir);
                 $stmp->bindParam(":name", $name);
                 $stmp->bindParam(":chmod", $basicmod, \PDO::PARAM_INT);
                 $stmp->bindParam(":owner", $data->user->idterminal_user);
