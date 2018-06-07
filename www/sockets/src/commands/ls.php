af0be29d75dc56ceec9db9db5ffa5d0724198518
Mickal decelle <mickaeldecelle@hotmail.fr>
2018-06-07 12:34:45 +0200

    Creating ls -l.
    Creating ls -l.

--- www/sockets/src/commands/ls.php
+++ www/sockets/src/commands/ls.php
@@ -46,6 +46,7 @@ class ls implements CommandInterface
     public static function call(\PDO $db, \SplObjectStorage $clients, SenderData &$data, ConnectionInterface $sender, string $sess_id, array $sender_session, string $terminal_mac, string $cmd, $parameters, bool &$lineReturn)
     {
         $str = "";
+        $lineReturn = false;
         //Get the curent id form the actual position of the user in a var
         $currentPath = CommandAsset::getIdDirectory($db, $terminal_mac, $data->position);
 
@@ -55,32 +56,52 @@ class ls implements CommandInterface
         //Get the dirs in the actual directory in an array
         $dirs = CommandAsset::getDirectories($db, $terminal_mac, $currentPath);
 
-        if ($files !== null || $dir !== null) {
-            $str = $str . "<br><div class='container flex' style='flex:wrap; padding: 0;'>";
+        if (!empty($parameters)) {
+            $options = CommandAsset::getOptions($parameters);
         }
 
-        //Return the files and the dirs to the user
-        foreach ($files as $file) {
-            $chmod = CommandAsset::getChmod($db, $terminal_mac, $file);
-            if ($chmod == 777) {
-                $str = $str . '<span style="padding-left: 0; padding-top: 20px; padding-right:20px;"><span style="color:yellow;">' . $file . '</span></span>';
-            } else {
-                $str = $str . '<span style="padding-left: 0; padding-top: 20px; padding-right:20px;">' . $file . '</span>';
+        if (empty($options)) {
+            if ($files !== null || $dir !== null) {
+                $str = $str . "<br><div class='container flex' style='flex:wrap; padding: 0;'>";
             }
-        }
-        foreach ($dirs as $dir) {
-            $chmod = CommandAsset::getChmod($db, $terminal_mac, $dir);
-            if ($chmod == 777) {
-                $str = $str . '<span style="padding-left: 0; padding-top: 20px; padding-right:20px;"><span style="color:blue; background-color:green;">' . $dir . ' </span></span>';
-            } else {
-                $str = $str . '<span style="color:blue; padding-left: 0; padding-top: 20px; padding-right:20px;">' . $dir . ' </span>';
+            foreach ($files as $file) {
+                $chmod = CommandAsset::getChmod($db, $terminal_mac, $file->name);
+                if ($chmod == 777) {
+                    $str = $str . '<span style="padding-left: 0; padding-top: 20px; padding-right:20px;"><span style="color:yellow;">' . $file->name . '</span></span>';
+                } else {
+                    $str = $str . '<span style="padding-left: 0; padding-top: 20px; padding-right:20px;">' . $file->name . '</span>';
+                }
             }
-        }
+            foreach ($dirs as $dir) {
+                $chmod = CommandAsset::getChmod($db, $terminal_mac, $dir->name);
+                if ($chmod == 777) {
+                    $str = $str . '<span style="padding-left: 0; padding-top: 20px; padding-right:20px;"><span style="color:blue; background-color:green;">' . $dir->name . ' </span></span>';
+                } else {
+                    $str = $str . '<span style="color:blue; padding-left: 0; padding-top: 20px; padding-right:20px;">' . $dir->name . ' </span>';
+                }
+            }
+            if ($files !== null || $dir !== null) {
+                $str = $str . '</div>';
+            }
+            $sender->send("message|" . $str);
+        } else if (\in_array("l", $options)) {
+            if ($files !== null || $dir !== null) {
+                $str = $str . "<br><tr style='padding: 0;'>";
+            }
+            //Return the files and the dirs to the user
+            foreach ($files as $file) {
+                $chmod = CommandAsset::getChmod($db, $terminal_mac, $file->name);
+                $str = $str . '<td style="padding-right: 0;">' . $file->chmod . $file->chmod . $file->owner . $file->owner . $file->owner . $file->editeddate . $file->name . '</td><br>';
+            }
+            foreach ($dirs as $dir) {
+                $chmod = CommandAsset::getChmod($db, $terminal_mac, $dir->name);
+                $str = $str . '<td style="padding-right: 0;">' . $dir->chmod . $dir->chmod . $dir->owner . $dir->owner . $dir->owner . $dir->editeddate . $dir->name . ' </td><br>';
+            }
 
-        if ($files !== null || $dir !== null) {
-            $str = $str . '</div>';
+            if ($files !== null || $dir !== null) {
+                $str = $str . '</tr>';
+            }
+            $sender->send("message|" . $str);
         }
-
-        $sender->send("message|" . $str);
     }
 }
