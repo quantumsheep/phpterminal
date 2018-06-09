8b2fa42d3584e1b23ad3569a6abcbd1de775f29e
Simonpo95 <33205321+Simonpo95@users.noreply.github.com>
2018-06-09 23:12:52 +0200

    locate and chmod commands + fixes
    * mkdir is alive
    
    * true mkdir commit
    
    * mkdir patchs
    
    * has to
    
    * has to 2
    
    * vla
    
    * Auto stash before merge of "master" and "QuantumSheep/master"
    
    * relative mkdir
    
    * cd mkdir upgraded
    
    * empty ls, mkdir fixed, cd upgraded
    
    * Building purefect mkdir
    
    * mkdir updated
    
    * ls partial mkdir upgraded cd upgraded
    
    * fonctionnal ls
    
    * final commit Simon
    
    * Auto stash before merge of "master" and "Simonpo95/master"
    
    * Update start_sockets.bat
    
    * Update start_sockets.bat
    
    * cd upgraded
    
    * Commit touch in dev
    
    * DICK
    
    This is my PR.
    Deal with it <3
    
    * Adding functionnal touch
    
    * cd + Helpers fixed
    
    * Auto stash before merge of "master" and "QuantumSheep/master"
    
    * Adding Pre_ls Pres_rm and some improvements to touch.
    
    * Improvements for touch and ls
    
    * Change class name and description
    
    * commité du mkdir
    
    * Adding 100% working touch/ls/rm base code.
    
    Adding 100% working touch/ls/rm base code.
    
    * Upgrading History
    
    Adding -c and -d
    
    * new route, optionAccount, controller fixed
    
    * Adding comments and reformat History/ls/touch
    
    Adding comments and reformat History/ls/touch
    
    * Auto stash before merge of "master" and "mickdec/master"
    
    CommandAsset deployed
    
    * Adding back helpers
    
    * merge before bdd changement
    
    * Correct JS bugs
    
    Correct JS bugs
    
    * CommandAsset Deployment
    
    * Adding touch with new assets
    
    Adding touch with new assets
    
    * Removing launch.js
    
    Removing launch.js
    
    * co
    
    * Adding rm with new assets
    
    Adding rm with new assets
    
    * removed Command Asset
    
    * Adding ls with new assets
    
    Adding ls with new assets
    
    * Auto stash before merge of "master" and "QuantumSheep/master"
    
    * Adding color for ls. Fixing move bug of JS. Removing some var_dump. Starting mv with new assets
    
    Adding color for ls. Fixing move bug of JS. Removing some var_dump. Starting mv with new assets
    
    * Finishing ls base command.
    
    Finishing ls base command.
    
    * Creating ls -l.
    
    Creating ls -l.
    
    * Fix editeddate declaration in Terminal_File model
    
    * ls -l
    
    ls -l
    
    * Finilizing command
    
    Finilizing command
    
    * Adding rmdir + Improving rm.
    
    Adding rmdir + Improving rm.
    
    * Auto stash before merge of "master" and "QuantumSheep/master"
    
    * cd quick fix + mkdir commentary and var_dump removed
    
    * Addind parentid parameter for getChmod
    
    Addind parentid parameter for getChmod
    
    * commit before merge
    
    * Merge branch 'master' into QuantumSheep/master
    
    aa
    
    * Auto stash before merge of "master" and "Simonpo95/master"

---
+++ www/sockets/src/commands/locate.php
@@ -0,0 +1,49 @@
+<?php
+namespace Alph\Commands;
+
+use Alph\Services\CommandAsset;
+use Alph\Services\CommandInterface;
+use Alph\Services\SenderData;
+use Ratchet\ConnectionInterface;
+
+class locate implements CommandInterface
+{
+    const USAGE = "mkdir [OPTION]... DIRECTORY...";
+
+    const SHORT_DESCRIPTION = "Create the DIRECTORY(ies), if they do not already exist.";
+    const FULL_DESCRIPTION = "Create the DIRECTORY(ies), if they do not already exist. If paths are provided, do NOT create directory if the path provided is wrong.";
+
+    const OPTIONS = [
+        "-p" => "Create directory from paths provided in case of the directories doesn't exist",
+    ];
+
+    const EXIT_STATUS = "Returns exit status of command or success if command is null.";
+
+    /**
+     * Call the command
+     *
+     * @param \PDO $db
+     * @param \SplObjectStorage $clients
+     * @param ConnectionInterface $sender
+     * @param string $sess_id
+     * @param string $cmd
+     */
+    public static function call(\PDO $db, \SplObjectStorage $clients, SenderData &$data, ConnectionInterface $sender, string $sess_id, array $sender_session, string $terminal_mac, string $cmd, $parameters, bool &$lineReturn)
+    {
+        $path = [];
+
+        // locate by himself return an error
+        if (empty($parameters)) {
+            return $sender->send("message|<br> You must enter a file or a directory to be found");
+        }
+
+
+        $fullNames = CommandAsset::getDirFileName($parameters, $data->position);
+
+        if(isset($fullNames[1])){
+            return $sender->send("message|<br> multiple argument entered. Locate only support one argument");
+        }
+
+        CommandAsset::locateFile($db, $fullNames, $terminal_mac);
+    }
+}
